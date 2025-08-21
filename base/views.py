5311123c18996b628eea3f03df38d7214d4a7786
kamal <kamalchowary@gmail.com>
2025-08-21 18:30:43 -0400

    room changes1

--- base/views.py
+++ base/views.py
@@ -42,7 +42,7 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context={'room':room}
+    context = {'room':room}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='/login')
