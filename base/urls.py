fcc119df3b366ef5452e3b5d049fe9d2b3788847
kamal <kamalchowary@gmail.com>
2025-08-22 21:27:11 -0400

    delete message

--- base/urls.py
+++ base/urls.py
@@ -9,5 +9,6 @@ urlpatterns=[
     path('room/<str:pk>/', views.room,name='room'),
     path('create-room/',views.createroom,name='create-room'),
     path('update-room/<str:pk>',views.updateroom,name='update-room'),
-    path('delete-room/<str:pk>',views.deleteroom,name='delete-room')
+    path('delete-room/<str:pk>',views.deleteroom,name='delete-room'),
+    path('delete-message/<str:pk>',views.deletemessage,name='delete-message')
 ]
