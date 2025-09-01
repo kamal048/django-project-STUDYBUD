341a0d7dff516f3dcccb097e19bfe8483fdb1525
kamal <kamalchowary@gmail.com>
2025-08-31 23:16:27 -0400

    Custom User Model

--- base/views.py
+++ base/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .models import Room, Topic,Message
-from .forms import RoomForm,UserForm
+from .models import Room, Topic, Message, User
+from .forms import RoomForm,UserForm, MyUserCreationForm
 
 # Create your views here.
 #rooms=[
@@ -36,9 +34,9 @@ def loginpage(request):
     return render(request,'base/login_register.html',context)
 
 def registerpage(request):
-    form=UserCreationForm()
+    form=MyUserCreationForm()
     if request.method=='POST':
-        form=UserCreationForm(request.POST)
+        form=MyUserCreationForm(request.POST)
         if form.is_valid():
             user=form.save(commit=False)
             user.username=user.username.lower()
@@ -148,7 +146,7 @@ def updateuser(request):
     user=request.user
     form=UserForm(instance=user)
     if request.method=='POST':
-        form=UserForm(request.POST,instance=user)
+        form=UserForm(request.POST,request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile',pk=user.id)
--- proj1/settings.py
+++ proj1/settings.py
@@ -26,8 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+    'rest_framework',
 ]
+
+AUTH_USER_MODEL= 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -106,11 +109,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL='/images/'
 
 STATICFILES_DIRS=[
     BASE_DIR/'static'
 ]
+
+MEDIA_ROOT= BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
